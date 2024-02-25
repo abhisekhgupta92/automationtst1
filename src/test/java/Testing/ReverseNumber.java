68978b11b97e90bddf92fe3249ed4d811064b301
abhisekhgupta92 <abhisekhgupta92@gmail.com>
2024-02-25 22:58:24 +0530

    Reverse class push

--- src/test/java/Testing/ReverseNumber.java
+++ src/test/java/Testing/ReverseNumber.java
@@ -3,7 +3,18 @@ package Testing;
 public class ReverseNumber {
 	
 	public static void main(String[] args)   
-	{ System.out.println("ReverseNumber");
+	
+	{	
+	int number = 34768, reverse = 0;
+	while(number!=0){
+		int reminder = number%10;
+		reverse = reverse*10 + reminder;
+		number = number/10;
+	}
+	
+	
+	
+	System.out.println("ReverseNumber of given number is " + reverse);
 	
 	}
 
